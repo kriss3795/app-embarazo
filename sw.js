af0a88ee76f01ccfdbb95ca1f272ff710ce27a1f
kriss3795 <cpradop1995@gmail.com>
2026-09-09 17:15:43 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+// Service worker: cachea todo para que funcione sin internet
+const CACHE = 'vale-guagua-v2';
+const ASSETS = ['./', './index.html', './manifest.json',
+  './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      // guarda fuentes de Google al vuelo para verlas offline
+      if (e.request.url.includes('fonts.g')) {
+        const cp = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, cp));
+      }
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
